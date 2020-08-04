76bfe25cbb34a41a1904a60faf72e4c43b8c97f5
abhusnurmath <bhusnur@gmail.com>
2020-08-04 15:55:47 -0400

    added some scanner stuff

--- src/HW.java
+++ src/HW.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
 
 public class HW {
   
   public static void main(String[] args) {
     System.out.println("hello, Arvind");
+    Scanner s = new Scanner(System.in);
+    if (s.next() == "abcd") {
+      //do something;
+    }
+    s.close();
   }
 
 }
