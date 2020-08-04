39dc38347aa5103dac0098a911316fd32214dfe4
abhusnurmath <bhusnur@gmail.com>
2020-08-04 16:05:14 -0400

    conflict attempt

--- src/HW.java
+++ src/HW.java
@@ -6,7 +6,8 @@ public class HW {
     System.out.println("hello, Arvind");
     Scanner s = new Scanner(System.in);
     if (s.next() == "abcd") {
-      //do something;
+      int x = 5;
+      System.out.println(x);
     }
     s.close();
   }
